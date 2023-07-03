fc2b4bc3c3429533c041b8590ccc757719dfeb9a
baovg1 <giabaovo123@gmail.com>
2023-07-03 10:58:04 +0700

    config cicd

--- build/userdata.sh
+++ build/userdata.sh
@@ -3,5 +3,5 @@ sudo yum update -y
 sudo yum install -y docker
 sudo service docker start
 sudo usermod -a -G docker ec2-user
-sudo docker pull giabaohb48/mlops-practice:latest
-sudo docker run --name mlops-practice -p 3000:3000 -d mlops-practice
+sudo docker pull 749971312816.dkr.ecr.us-west-1.amazonaws.com/mlops-practice:latest
+sudo docker run --name mlops-practice -p 3000:3000 -d mlops-practice:latest
