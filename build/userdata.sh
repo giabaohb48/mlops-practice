19b7043ff932c521957a8842d3cdf86d1d17bb27
baovg1 <giabaovo123@gmail.com>
2023-07-03 10:48:48 +0700

    config pipeline cicd

---
+++ build/userdata.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+sudo yum update -y
+sudo yum install -y docker
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+sudo docker pull giabaohb48/mlops-practice:latest
+sudo docker run --name mlops-practice -p 3000:3000 -d mlops-practice
