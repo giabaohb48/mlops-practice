87f9f2dffe16eb617952b384ae20e07ba06cbf9e
baovg1 <giabaovo123@gmail.com>
2023-07-04 09:56:33 +0700

    fix

--- build/userdata.sh
+++ build/userdata.sh
@@ -3,4 +3,4 @@ sudo yum update -y
 sudo yum install -y docker
 sudo service docker start
 sudo usermod -a -G docker ec2-user
-sudo docker run --name mlops-practice -p 3000:3000 -d giabaohb48/mlops-practice:latest
+sudo docker run --name mlops-practice -p 5000:5000 -d giabaohb48/mlops-practice:latest
